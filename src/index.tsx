547060803ab300838fbc37b42b793334130a5bf4
Agleylson Melo <agleylsonmelo@outlook.com>
2021-06-25 18:43:41 -0300

    Chore: configure PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Modal from 'react-modal'
+import Modal from "react-modal";
 import App from "./App";
+
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 import "./services/firebase";
 
-Modal.setAppElement("#root")
+Modal.setAppElement("#root");
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,3 +16,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
